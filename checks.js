bca3b6cd6a7ea598826868fde4008ac2e53e8317
= <=>
2023-02-28 20:29:07 +0000

    Added a check for key value objects.

--- checks.js
+++ checks.js
@@ -137,3 +137,15 @@ export function checkIfArray(arg) {
         throw TypeError("Not an array");
     }
 };
+
+
+
+export function isKeyValueObject(arg) {
+    return arg.constructor === Object && isClass(arg) === false;
+};
+
+export function checkIfKeyValueObject(arg) {
+    if (isKeyValueObject(arg) === false) {
+        throw TypeError("Not an object");
+    }
+};
