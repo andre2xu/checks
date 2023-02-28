aafe78a5263280d41beb1412875300b2a762c0f5
= <=>
2023-02-28 18:32:06 +0000

    Added a check for booleans.

--- checks.js
+++ checks.js
@@ -61,3 +61,15 @@ export function checkIfZero(arg) {
         throw TypeError("Not zero");
     }
 };
+
+
+
+export function isBoolean(arg) {
+    return typeof arg === 'boolean';
+};
+
+export function checkIfBoolean(arg) {
+    if (isBoolean(arg) === false) {
+        throw TypeError("Not a boolean");
+    }
+};
