047a5b1fb9485e8649d060733261a233fb3bfa70
= <=>
2023-03-02 14:47:52 +0000

    Created checks.js that places all checking helpers into one object called 'checks'.

--- checks.js
+++ checks.js
@@ -1,31 +1,6 @@
-import * as data_struct_checks from './core/data_structures.js';
-import * as non_prim_checks from './core/non_primitives.js';
-import * as num_checks from './core/numbers.js';
+import * as nprim_checks from './core/non_primitives.js';
 import * as prim_checks from './core/primitives.js';
+import * as ds_checks from './core/data_structures.js';
+import * as num_checks from './core/numbers.js';
 
-
-
-const IMPORTS = [
-    data_struct_checks,
-    non_prim_checks,
-    num_checks,
-    prim_checks
-];
-const NUM_OF_IMPORTS = IMPORTS.length;
-
-
-
-var checks = {};
-
-for (let i=0; i < NUM_OF_IMPORTS; i++) {
-    const CHECKS_ARRAY = Object.values(IMPORTS[i]);
-    const NUM_OF_CHECKS = CHECKS_ARRAY.length;
-
-    for (let j=0; j < NUM_OF_CHECKS; j++) {
-        const CHECK = CHECKS_ARRAY[j];
-
-        checks[CHECK.name] = CHECK;
-    }
-}
-
-export default checks;
+export var checks = Object.assign({}, nprim_checks, prim_checks, ds_checks, num_checks);
